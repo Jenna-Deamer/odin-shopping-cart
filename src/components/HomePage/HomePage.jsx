19baa935fbeabca50a22c156a61b824ff4d8f2f9
Jenna-Deamer <jennadeamer@gmail.com>
2025-11-02 19:53:59 -0500

    Add category links to home

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom";
+import styles from './Home.module.css';
 function HomePage() {
 
-  return (
-    <>
-    <h1>Welcome</h1>
-    </>
-  
-  )
+    return (
+        <>
+            <h1>Welcome</h1>
+
+            <div className={styles.categoriesContainer}>
+                <Link to="/shop">Shop All</Link>
+                <Link to="/shop/women">Shop Women's</Link>
+                <Link to="/shop/men">Shop Men's</Link>
+
+            </div>
+        </>
+
+    )
 }
 
 export default HomePage
